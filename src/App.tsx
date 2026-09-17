f7fc9ce6166fe3fbc4438ba2abab59342f54f354
FATHIDDIN <161296793+IceCremas@users.noreply.github.com>
2026-09-17 14:03:21 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+export default function App(){
+  return(
+    <>
+    <header className="header">
+      <h1>Harry Potter</h1>
+      <p>frlgnjegnoeger</p>
+      <input type="text"/>
+    </header>
+    </>
+  )
+}
